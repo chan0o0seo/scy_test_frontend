d5d75197c1cdb5c6d787fd9fc7babb3320578cef
scy <tjcksdud0221@naver.com>
2025-02-26 14:18:11 +0900

    상세페이지 디자인 추가

--- src/components/BoardDetail.vue
+++ src/components/BoardDetail.vue
@@ -1,57 +1,82 @@
 <template>
-    <div>
-        <h1>게시글 상세</h1>
+    <div class="board-detail">
         <div v-if="board">
-            <h2>{{ board.title }}</h2>
-            <p>{{ board.content }}</p>
-            <p><strong>Writer:</strong> {{ board.writer }}</p>
 
-            <div v-if="board.comments && board.comments.length">
-                <h3>댓글</h3>
-                <div v-for="comment in board.comments" :key="comment.idx">
-                    <p><strong>{{ comment.writer }}:</strong> {{ comment.content }}</p>
-                </div>
+            <h2>제목 {{ board.title }}</h2>
+            <v-row class="mb-6" no-gutters>
+                <v-col cols="3" offset="3">
+                    <v-sheet class="pa-2 ma-2">
+                        <p><strong>Writer:</strong> {{ board.writer }}</p>
+                    </v-sheet>
+                </v-col>
+            </v-row>
+
+            <v-divider></v-divider>
+            <div class="board-content">
+
+                <p>{{ board.content }}</p>
             </div>
-            <div v-else>
-                <p>댓글이 없습니다.</p>
-            </div>
-        </div>
-        <div>
-            <router-link :to="{ name: 'board_list' }">목록</router-link>
-            <v-sheet class="mx-auto" width="300">
+
+            <v-divider></v-divider>
+            
+            <div style="display: flex;">
+            
+            <h3>댓글 작성</h3>
+
+            <v-sheet class="mx-auto" width="300" >
                 <v-form @submit.prevent="submit">
                     <v-text-field v-model="writer" label="작성자"></v-text-field>
-                    <v-textarea v-model="content" label="내용" variant="outlined" @keyup.enter="submit" ></v-textarea>
+                    <v-textarea v-model="content" label="내용" variant="outlined" @keyup.enter="submit"></v-textarea>
                     <v-btn class="mt-2" type="submit" block>제출</v-btn>
                 </v-form>
             </v-sheet>
         </div>
+            
+            <div v-if="board.comments && board.comments.length">
+                <div v-for="(comment,index) in board.comments" :key="comment.idx" class="comment-container">
+                    <p>   <strong>작성자: {{ comment.writer }}</strong> </p>
+                    <div class="comment-content">
+                        {{ comment.content }}
+                    </div>
+                </div>
+            </div>
+            <div v-else class="comment-container">
+                <p>댓글이 없습니다.</p>
+            </div>
+        </div>
+
+        <v-btn class="mt-2" @Click = goList> 목록</v-btn>
+
     </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute,useRouter } from 'vue-router'
 import api from '../api'
 
-const route = useRoute()
+const route = useRoute();
+const router = useRouter();
 const board = ref(null);
 const writer = ref();
 const content = ref();
 const submit = async () => {
-  try {
-    const response = await api.writeComment(board.value.idx, writer.value, content.value);
-    console.log('댓글 작성 성공:', response);
-    const boardIdx = route.params.idx
-    const boardResponse = await api.getBoardDetail(boardIdx)
-    board.value = boardResponse.data // 새로 가져온 게시글과 댓글 목록으로 갱신
+    try {
+        const response = await api.writeComment(board.value.idx, writer.value, content.value);
+        console.log('댓글 작성 성공:', response);
+        const boardIdx = route.params.idx
+        const boardResponse = await api.getBoardDetail(boardIdx)
+        board.value = boardResponse.data // 새로 가져온 게시글과 댓글 목록으로 갱신
 
-    // 입력값 초기화
-    writer.value = ''
-    content.value = ''
-  } catch (error) {
-    console.log('댓글 작성 실패:', error);
-  }
+        // 입력값 초기화
+        writer.value = ''
+        content.value = ''
+    } catch (error) {
+        console.log('댓글 작성 실패:', error);
+    }
+}
+const goList = () => {
+    router.push({ name: 'board_list' })
 }
 onMounted(async () => {
     const boardIdx = route.params.idx
@@ -61,4 +86,28 @@ onMounted(async () => {
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.title-container {
+    display: flex;
+}
+
+.board-detail {
+    min-width: 1000px;
+    justify-content: center;
+}
+
+.board-content {
+    font-size: x-large;
+    padding: 100px;
+}
+.comment-container {
+    padding: 10px;
+    margin-top: 10px;
+}
+.comment-content {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: large;
+}
+</style>
